154c70db76e8e3c7ed6a150013457bf286621a07
Andrii Bilmak <bilmak14@gmail.com>
2022-02-16 00:43:12 +0200

    Создание компонентов из html разметки

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  RENT_COUNT: 3,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      rentCount = {Setting.RENT_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
